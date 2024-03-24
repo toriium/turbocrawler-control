2a4e6f0920ca809fc3d144486ebaf410d25642df
toriium <zezim@gmail.com>
2024-03-24 18:14:58 +0000

    feat: add tests

---
+++ tests/test.py
@@ -0,0 +1,15 @@
+from tests.test_crawlers.quotes_crawler import QuotesToScrapeCrawler
+from turbocrawler import CrawlerRunner
+from turbocrawler.engine.data_types.crawler_runner_config import CrawlerRunnerConfig
+from turbocrawler.queues.crawled_queue import MemoryCrawledQueue
+from turbocrawler.queues.crawler_queues import FIFOMemoryCrawlerQueue
+from turbocrawler_control.plugin.plugin import TurboCrawlerPlugin
+
+if __name__ == '__main__':
+    config = CrawlerRunnerConfig(crawler_queue=FIFOMemoryCrawlerQueue,
+                                 crawler_queue_params=None,
+                                 crawled_queue=MemoryCrawledQueue,
+                                 crawled_queue_params=dict(save_crawled_queue=True, load_crawled_queue=False),
+                                 plugins=[TurboCrawlerPlugin], qtd_parse=2)
+    result = CrawlerRunner(crawler=QuotesToScrapeCrawler, config=config).run()
+    print(result)
