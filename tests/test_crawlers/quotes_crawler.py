45d034bb92e8d2a50fd94680f51dd63425b35f9d
toriium <MY_NAME@example.com>
2024-04-14 16:13:29 +0100

    new model

--- tests/test_crawlers/quotes_crawler.py
+++ tests/test_crawlers/quotes_crawler.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 
 import requests
 from selectolax.lexbor import LexborHTMLParser
@@ -8,31 +9,32 @@ from turbocrawler import Crawler, CrawlerRequest, CrawlerResponse, ExecutionInfo
 class QuotesToScrapeCrawler(Crawler):
     crawler_name = "QuotesToScrape"
     allowed_domains = ['quotes.toscrape.com']
-    regex_extract_rules = []
+    regex_extract_rules = [ExtractRule(r'https://quotes.toscrape.com/page/[0-9]')]
     time_between_requests = 1
     session: requests.Session
 
-    @classmethod
-    def start_crawler(cls) -> None:
-        cls.session = requests.session()
+    def start_crawler(self) -> None:
+        self.session = requests.session()
+        self.aaaa= 4
 
-    @classmethod
-    def crawler_first_request(cls) -> CrawlerResponse | None:
-        cls.crawler_queue.add(CrawlerRequest(url="https://quotes.toscrape.com/page/9/"))
-        response = cls.session.get(url="https://quotes.toscrape.com/page/1/")
+    def crawler_first_request(self) -> CrawlerResponse | None:
+        plugin = self.get_plugin("TestPlugin")
+        self.crawler_queue.add(CrawlerRequest(url="https://quotes.toscrape.com/page/9/"))
+        response = self.session.get(url="https://quotes.toscrape.com/page/1/")
         return CrawlerResponse(url=response.url,
                                body=response.text,
                                status_code=response.status_code)
 
-    @classmethod
-    def process_request(cls, crawler_request: CrawlerRequest) -> CrawlerResponse:
-        response = cls.session.get(crawler_request.url)
+    def process_request(self, crawler_request: CrawlerRequest) -> CrawlerResponse:
+        response = self.session.get(crawler_request.url)
         return CrawlerResponse(url=response.url,
                                body=response.text,
                                status_code=response.status_code)
 
-    @classmethod
-    def parse(cls, crawler_request: CrawlerRequest, crawler_response: CrawlerResponse) -> None:
+    def process_response(self, crawler_request: CrawlerRequest, crawler_response: CrawlerResponse) -> None:
+        crawler_response.kwargs['success'] = True
+
+    def parse(self, crawler_request: CrawlerRequest, crawler_response: CrawlerResponse) -> None:
         selector = LexborHTMLParser(crawler_response.body)
         quote_list = selector.css('div[class="quote"]')
         for quote in quote_list:
@@ -41,6 +43,5 @@ class QuotesToScrapeCrawler(Crawler):
                     "tag_list": [tag.text() for tag in quote.css('div[class="tags"]>a') if tag]}
             # pprint(data)
 
-    @classmethod
-    def stop_crawler(cls, execution_info: ExecutionInfo) -> None:
-        cls.session.close()
+    def stop_crawler(self, execution_info: ExecutionInfo) -> None:
+        self.session.close()
